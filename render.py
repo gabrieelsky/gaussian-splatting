370f4489e2a942c36ce6d6caea48fb5d22461414
gabrieelsky <129524998+gabrieelsky@users.noreply.github.com.>
2026-04-03 12:48:26 +0200

    Save depth as .npy

--- render.py
+++ render.py
@@ -9,6 +9,11 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+########## ADDED ###########
+import numpy as np
+############################
+
+
 import torch
 from scene import Scene
 import os
@@ -26,16 +31,22 @@ try:
 except:
     SPARSE_ADAM_AVAILABLE = False
 
-
+######## MODIFIED #########
 def render_set(model_path, name, iteration, views, gaussians, pipeline, background, train_test_exp, separate_sh):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
     gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+    depth_path = os.path.join(model_path, name, "ours_{}".format(iteration), "depth")
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
+    makedirs(depth_path, exist_ok=True)
 
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        rendering = render(view, gaussians, pipeline, background, use_trained_exp=train_test_exp, separate_sh=separate_sh)["render"]
+        render_pkg = render(view, gaussians, pipeline, background, use_trained_exp=train_test_exp, separate_sh=separate_sh)
+        
+        rendering = render_pkg["render"]
+        depth = render_pkg["depth"]
+        
         gt = view.original_image[0:3, :, :]
 
         if args.train_test_exp:
@@ -44,6 +55,8 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
 
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+        np.save(os.path.join(depth_path, '{0:05d}'.format(idx) + ".npy"), depth.cpu().numpy())
+############################
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, separate_sh: bool):
     with torch.no_grad():
